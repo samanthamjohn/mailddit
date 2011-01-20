0f0221f207bf1d279b6fc0be86d222ba437e7f38
Samantha M. John <samjohn@Macintosh-d49a20e658e8-104.local>
2011-01-20 14:36:52 -0500

    adding new rails app

---
+++ mailddit_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaildditRails::Application.config.secret_token = 'e37d386ed1fe16939611a9f26a826abc305c2a010297ab264e0493284b215c320fb162c72c9f172b1db418b3b1ee2d8562080aa1f6f1a377e47f93ec943fb342'
