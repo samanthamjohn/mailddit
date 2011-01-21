f0eb3caf0d541fb7bafc55c3b158a13373814d04
Samantha John <Sam@samantha-johns-macbook-3231.local>
2011-01-21 17:16:59 -0500

    commiting mailhandler code

---
+++ mailddit_rails/app/models/mail_handler.rb
@@ -0,0 +1,46 @@
+class MailHandler
+  attr_accessor :email, :text_body, :html_body, :tags, :date
+  def initialize
+  end
+  
+  def send_mail
+    mail = Mail.new do
+         from 'smj2118@columbia.edu'
+           to 'samantha.m.john@gmail.com'
+          subject 'This is a test email'
+         body 'body'
+    end
+     mail.deliver!
+    
+  end
+
+  def receive_mail
+    
+    Mail.defaults do
+      retriever_method :pop3, { :address             => "pop.gmail.com",
+                                :port                => 995,
+                                :user_name           => 'mailddit',
+                                :password            => 'samiscool',
+                                :enable_ssl          => true }
+    end
+    @email = Mail.last if Mail  #=> Returns the first unread email
+  end
+  
+  def parse_mail
+
+    @email.parts.each do |part|
+      content_type = part.content_type.split(";").first
+      body = part.body.raw_source
+      if content_type == "text/plain"
+        @text_body = body
+      elsif content_type == "text/html"
+        @html_body = body
+      end
+    end
+    subject = @email.subject.gsub("Fwd: ", " ").gsub("Re: ", " ")
+    @tags = subject.split(",").collect{|subj| subj.strip}
+    @date = @email.date
+  end
+  
+end
+
